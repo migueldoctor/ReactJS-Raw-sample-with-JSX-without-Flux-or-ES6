881ad0c1366ddbaca2c4e61b55ffe038c48eb3ad
migueldoctor <mikydoc_sev@hotmail.com>
2017-08-22 15:09:59 +0200

    Adding Styles, ContactView Component and Clean up the Code

--- main.jsx
+++ main.jsx
@@ -1,48 +1,24 @@
-   // rootElement creation without JSX
-      /*
-        var rootElement = React.createElement('div',{},
-         React.createElement('h1',{},"Contacts"),
-         React.createElement('ul',{},
-           React.createElement('li', {},
-               React.createElement('h2', {}, "James Nelson"),
-               React.createElement('a', {href: 'mailto:james@jamesknelson.com'}, 'james@jamesknelson.com')
-             ),
-             React.createElement('li', {},
-               React.createElement('h2', {}, "Joe Citizen"),
-               React.createElement('a', {href: 'mailto:joe@example.com'}, 'joe@example.com')
-             )
-           )
-         );
-        );
-      */
-
-      // rootElement creation with JSX
-      /*var rootElement = <div>
-                              <h1>Contacts</h1>
-                              <ul>
-                                <li><h2>James Nelson</h2>
-                                    <a href='mailto:migueldoctor@gmail.com'>migueldoctor@gmail.com</a>
-                                </li>
-                                <li><h2>Joe Citizen</h2>
-                                    <a href='mailto:joe@example.com'>joe@example.com</a>
-                                </li>
-                              </ul>
-                        </div>
-       */
-
-      // Since React and JSX are js compatible we can use it in loops, if statements or any other feature provided by javascript
-      //4.1) Let us create an array JSON objects wth users and optional emails.
-      //Now we add as well description fields to some of the contacts elements
+/******************************************* 
+ * DATA SECTION
+ ******************************************/
       var contacts = [
         {key: 1, name:'Miguel Doctor', email:'migueldoctor@gmail.com', description:'Miguel is the one making this example, so for that reason it will have a description field'},
         {key: 2, name:'Bob', description:'Bob is a great user but without email, so for that reason we provide him with a description'},
         {key: 3, name:'Joe Citizen',email:'mailto:joe@example.co'}
       ];
 
-       // Then we create a new empty contact object
-       var newContact = {name: "", email: "", description: ""}
-      
-      //5a) Here we create our first component with react and JSX
+      // Then we create a new empty contact object
+      var newContact = {name: "", email: "", description: ""}
+
+
+/******************************************* 
+ * COMPONENTS CREATION SECTION 
+ ******************************************/
+
+/*************************** 
+ ** ContactItem component
+ ***************************/
+
       var ContactItem = React.createClass({
         propTypes:{ //The passed argument must be a js object with name,email and description. This section is optional and can be infered by React by the JSX code in the render function
           //This way to define the proptypes is deprecated but we leave it in this way for learning porpuses
@@ -51,51 +27,68 @@
           description:React.PropTypes.string
         },
         render: function () {
-          return <li key={this.props.key}>
-                    <h2>{this.props.name}</h2>
-                    <a href={'mailto:'+this.props.email}>{this.props.email}</a>
-                    <div>{this.props.description}</div>
-                    </li>
+          // Wrong! There is no need to specify the key here: <li key={this.props.key}> because it only makes sense if you want to return an array variable so
+          // it should be placed in the return contained in the map function. See https://fb.me/react-warning-keys for more information
+          //return <li key={this.props.key}>
+          return  <li className='ContactItem'> 
+                    <h2 className='ContactItem-name'>{this.props.name}</h2>
+                    <a className='ContactItem-email' href={'mailto:'+this.props.email}>{this.props.email}</a>
+                    <div className='ContactItem-description'>{this.props.description}</div>
+                  </li>
         }
       });
 
-      // React Component ContactForm
+/*************************** 
+ ** ContactForm component
+ ***************************/
       var ContactForm = React.createClass({
         propTypes: {
           contact: React.PropTypes.object.isRequired
         },
-
         render: function(){
             return ( 
               <form className='ContactForm'>
-                <input type='text' placeholder='Name (required)' value={this.props.contact.name}/>
-                <input type='text' placeholder='Email (optional)' value={this.props.contact.email}/>
-                <input type='textarea' placeholder='Description (optional)' value={this.props.contact.description}/>
+                <input type='text' className='ContactItem-name' placeholder='Name (required)' value={this.props.contact.name}/>
+                <input type='text' className='ContactItem-email' placeholder='Email (optional)' value={this.props.contact.email}/>
+                <input type='textarea' className='ContactItem-description' placeholder='Description (optional)' value={this.props.contact.description}/>
                 <button type='submit'>Add Contact</button>
               </form>
             )
         }
       });
-
-      //4.2) Now let's filter the array by choosing the ones with email
-      var getEmailFromContact = function (contact) { return contact.email; }
       
-      //4.3) filter function applies a condition and return the elements of the array that returns not null
-      //map function applies the passed function to all elements of the array. 
+/*************************** 
+ ** ContactView component
+ ***************************/
+      var ContactView = React.createClass({
+        propTypes: {
+                contacts: React.PropTypes.array.isRequired,
+                newContact: React.PropTypes.object.isRequired,
+        },
 
-      //5b)In this case we return the ContactItem reactComponent we have just created
-      var listElements = contacts.filter(getEmailFromContact)
-                                 .map(function (contact) {
-                                     return <ContactItem name={contact.name} email={contact.email} description={contact.description}/>
-                                 })
+        render: function() {
+                var getEmailFromContact = function(contact) { return contact.email };
+                var contactItemElements = contacts.filter(getEmailFromContact)
+                                                  .map(function(contact) { // Correct! Key should be specified inside the array.See https://fb.me/react-warning-keys for more information.
+                                                                        return <ContactItem
+                                                                                     key={contact.key}
+                                                                                     name={contact.name} 
+                                                                                     email={contact.email} 
+                                                                                     description={contact.description}/>
+                                                                }
+                                                        );
 
-    // Finally we add the ContactForm component to our rootElement component
-    var rootElement = <div>
-                        <h1>Contacts</h1>
-                        <ul>
-                        {listElements}
-                        </ul>
-                        <ContactForm contact={newContact}/>
-                      </div>
+                return (
+                        <div className='ContactView'>
+                          <h1 className='ContactView-title'>Contacts</h1>
+                          <ul className='ContactView-list'>{contactItemElements}</ul>
+                          <ContactForm contact={this.props.newContact} />
+                        </div>
+                        )
+        }
+      });
 
-    ReactDOM.render(rootElement, document.getElementById('react-app'));
+/******************************************* 
+ * ENTRY POINT SECTION 
+ ******************************************/
+    ReactDOM.render(<ContactView contacts={contacts} newContact={newContact} />, document.getElementById('react-app'));
